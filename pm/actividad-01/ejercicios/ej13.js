acade07e6b408fe32d333569dcfb98f50be63717
Miguel Ángel <mabusta.06@gmail.com>
2025-09-19 14:09:12 +0200

    Add Ejercicio 13: Article component modifications

---
+++ pm/actividad-01/ejercicios/ej13.js
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar'; 
+import { StyleSheet, Text, View, Image } from 'react-native'; 
+
+export default function Article() {
+    let content = {
+        paragraphOne: "Texto 1",
+        paragraphTwo: "Texto 2",
+        title: "Titulo"
+    }
+
+    return (
+        <View style={styles.container}> 
+            <Text style={styles.title}>{content.title}</Text>
+            <Text>{content.paragraphOne}</Text>
+            <Text>{content.paragraphTwo}</Text>
+            <Image 
+                style={styles.logo}
+                source={{
+                    uri: 'https://reactnative.dev/img/tiny_logo.png',
+                }}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({ 
+  container: { 
+    flex: 1, 
+    backgroundColor: 'gray', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+  }, 
+  title: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    textDecorationLine: 'underline'
+  },
+  logo: {
+    width: 50,
+    height: 50,
+  }
+});
