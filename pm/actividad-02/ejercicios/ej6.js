2ee525451ca9ba5bf51086439f0d7841e95fce69
Miguel Angel <mabusta.06@gmail.com>
2025-09-19 16:14:32 +0200

    Add Ejercicio 6

---
+++ pm/actividad-02/ejercicios/ej6.js
@@ -0,0 +1,74 @@
+import { View, Pressable, Text, Image, StyleSheet } from 'react-native';
+import { useState } from 'react';
+
+export default function App() {
+  const [color, setColor] = useState('green');
+  const [text, setText] = useState('My Title');
+  const [image, setImage] = useState(require('./assets/splash-icon.png'));
+  const [btnColor, setBtnColor] = useState('yellow');
+  const [imgHeight, setImgHeight] = useState(300);
+  const [imgWidth, setImgWidth] = useState(300);
+
+  function handleOnPress() {
+    if (color == 'green') {
+      setColor('yellow');
+      setText('My New Title');
+      setImage(require('./assets/image-2.jpg'));
+      setBtnColor('green');
+    } else {
+      setColor('green');
+      setText('My Title');
+      setImage(require('./assets/splash-icon.png'));
+      setBtnColor('yellow');
+    }
+    setImgHeight(imgHeight - 20);
+    setImgWidth(imgWidth - 20);
+  }
+
+  return (
+    <View style={[styles.container, {backgroundColor : color}]}>
+      <Text style={styles.title}>{text}</Text>
+      <Image 
+        style={[
+          styles.image, 
+          {height : imgHeight},
+          {width : imgWidth}
+        ]} 
+        source={image} />
+      <Pressable onPress={handleOnPress}>
+        <Text style={[
+          styles.text, 
+          {backgroundColor : btnColor},
+        ]}>
+          Pulsame!
+        </Text>
+      </Pressable> 
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent:'center',
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    textDecorationLine: 'underline',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  text: {
+    height: 40,
+    width: 80,
+    backgroundColor: 'blue',
+    borderRadius: 8,
+    padding: 6,
+  },
+});
