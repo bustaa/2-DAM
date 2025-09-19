9c2fb2f86aa59b68b812c0d06a08f987619175b6
Miguel Ángel <mabusta.06@gmail.com>
2025-09-19 13:41:01 +0200

    Add Ejercicio 11

---
+++ pm/actividad-01/ejercicios/ej11.js
@@ -0,0 +1,47 @@
+import { StatusBar } from 'expo-status-bar'; 
+import { StyleSheet, Text, View } from 'react-native'; 
+
+export default function App() { 
+  let content = {
+    paragraphOne: "Texto 1",
+    paragraphTwo: "Texto 2",
+    title: "Titulo"
+  }
+
+  function Articles() {
+    return (
+      <View style={styles.container}> 
+      <Text style={styles.title}>{content.title}</Text>
+      <Text>{content.paragraphOne}</Text>
+      <Text>{content.paragraphTwo}</Text>
+    </View>
+    )
+  }
+
+  let arrArticles = [];
+
+  for (let i = 0; i < 4; i++) {
+    arrArticles.push(<Articles key={i.toString}/>)
+  }
+
+  return (
+    <>
+      {arrArticles}
+    </>
+  ); 
+} 
+
+const styles = StyleSheet.create({ 
+  container: { 
+    flex: 1, 
+    backgroundColor: 'gray', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+  }, 
+  title: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    textDecorationLine: 'underline'
+  },
+});
