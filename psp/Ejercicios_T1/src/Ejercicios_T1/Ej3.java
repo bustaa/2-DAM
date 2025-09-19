20e912e17620ec3e61b4f2a8b8bb37a7e32f6557
Miguel Angel <mabusta.06@gmail.com>
2025-09-19 18:30:27 +0200

    Add PSP T1 Ejercicio 3

---
+++ psp/Ejercicios_T1/src/Ejercicios_T1/Ej3.java
@@ -0,0 +1,12 @@
+package Ejercicios_T1;
+import java.util.Scanner;
+
+public class Ej3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1, n2;
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        System.out.println("Resultado: " + (n1+n2));
+    }
+}
