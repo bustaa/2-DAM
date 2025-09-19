78a234471aa7cb6b06dca9ba20febc178656c349
Miguel Angel <mabusta.06@gmail.com>
2025-09-19 18:09:02 +0200

    Add PSP T1 Ejercicio 1

--- psp/Ejercicios_T1/src/Ejercicios_T1/Ej1.java
+++ psp/Ejercicios_T1/src/Ejercicios_T1/Ej1.java
@@ -1,4 +1,11 @@
 package Ejercicios_T1;
 
 public class Ej1 {
+    public static void main(String[] args) {
+        int n1, n2;
+        n1 = 4;
+        n2 = 5;
+
+        System.out.println("Resultado: " + (n1 + n2));
+    }
 }
