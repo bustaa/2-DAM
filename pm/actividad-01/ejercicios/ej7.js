d0cf3ed2c04265fc3e4784309e1130b249911e04
Miguel Ángel <mabusta.06@gmail.com>
2025-09-19 13:32:26 +0200

    Add Ejercicio 7

---
+++ pm/actividad-01/ejercicios/ej7.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  let content = {
+    paragraphOne: "Texto 1",
+    paragraphTwo: "Texto 2",
+  }
+  return (
+    <View style={styles.container}>
+      <Text>{content.paragraphOne}</Text>
+      <Text>{content.paragraphTwo}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
