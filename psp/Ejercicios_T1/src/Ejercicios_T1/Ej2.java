9af6073dcb59c38e4858a79287114ada79209458
Miguel Angel <mabusta.06@gmail.com>
2025-09-19 18:25:20 +0200

    Add PSP T1 Ejercicio 2

---
+++ psp/Ejercicios_T1/src/Ejercicios_T1/Ej2.java
@@ -0,0 +1,11 @@
+package Ejercicios_T1;
+import java.util.Scanner;
+
+public class Ej2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nombre;
+        nombre = sc.nextLine();
+        System.out.println("Hola " + nombre);
+    }
+}
