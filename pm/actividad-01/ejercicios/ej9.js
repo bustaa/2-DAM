1034624177f4051bfc8088b38fafc5f48832711f
Miguel Ángel <mabusta.06@gmail.com>
2025-09-19 13:37:20 +0200

    Add Ejercicio 9

---
+++ pm/actividad-01/ejercicios/ej9.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  let content = {
+    paragraphOne: "Texto 1",
+    paragraphTwo: "Texto 2",
+    title: "Titulo"
+  }
+
+  function Article() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>{content.title}</Text>
+        <Text>{content.paragraphOne}</Text>
+        <Text>{content.paragraphTwo}</Text>
+      </View>
+    )
+  }
+
+  return (
+    <Article />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    textDecorationiLine: 'underline'
+  },
+});
