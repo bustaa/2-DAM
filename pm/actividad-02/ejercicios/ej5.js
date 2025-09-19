9070d4926518d568c0d421ec8337da832604f127
Miguel Angel <mabusta.06@gmail.com>
2025-09-19 16:06:18 +0200

    Add Ejercicio 5

---
+++ pm/actividad-02/ejercicios/ej5.js
@@ -0,0 +1,70 @@
+import { View, Pressable, Text, Image, StyleSheet } from 'react-native';
+import { useState } from 'react';
+
+export default function App() {
+  const [color, setColor] = useState('green');
+  const [text, setText] = useState('My Title');
+  const [image, setImage] = useState(require('./assets/splash-icon.png'));
+  const [btnColor, setBtnColor] = useState('yellow');
+  const [btnHeight, setBtnHeight] = useState(50);
+  const [btnWidth, setBtnWidth] = useState(50);
+
+  function handleOnPress() {
+    if (color == 'green') {
+      setColor('yellow');
+      setText('My New Title');
+      setImage(require('./assets/image-2.jpg'));
+      setBtnColor('green');
+      setBtnWidth(100);
+    } else {
+      setColor('green');
+      setText('My Title');
+      setImage(require('./assets/splash-icon.png'));
+      setBtnColor('yellow');
+      setBtnWidth(50  );
+    }
+  }
+
+  return (
+    <View style={[styles.container, {backgroundColor : color}]}>
+      <Text style={styles.title}>{text}</Text>
+      <Image style={styles.image} source={image} />
+      <Pressable onPress={handleOnPress}>
+        <Text style={[
+          styles.text, 
+          {backgroundColor : btnColor},
+          {height : btnHeight},
+          {width : btnWidth}
+        ]}>
+          Pulsame!
+        </Text>
+      </Pressable> 
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent:'center',
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    textDecorationLine: 'underline',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  text: {
+    height: 40,
+    width: 80,
+    backgroundColor: 'blue',
+    borderRadius: 8,
+    padding: 6,
+  },
+});
