d41af5373e86ff45a55e784dd0da2687735b0854
Miguel Angel <mabusta.06@gmail.com>
2025-09-25 17:10:34 +0200

    Add AD T1 Ejercicio1.2

---
+++ aadd/Ejercicios_T1/src/Ejercicios_T1/Ejercicio1_2.java
@@ -0,0 +1,23 @@
+package Ejercicios_T1;
+import java.io.File;
+
+public class Ejercicio1_2 {
+    public static void main(String args[]) {
+        File directorio = new File(args[0]);
+
+        System.out.println("Nombre: " + directorio.getName());
+        System.out.println("Ruta relativa: " + directorio.getPath());
+        System.out.println("Ruta absoluta: " + directorio.getAbsolutePath());
+        System.out.println("Existe: " + directorio.exists());
+        System.out.println("Permiso escritura: " + directorio.canWrite());
+        System.out.println("Permiso lectura: " + directorio.canRead());
+        System.out.println("Tamaño: " + directorio.length());
+
+        System.out.println("Contenido: ");
+        String[] listaContenido = directorio.list();
+
+        for (String e : listaContenido) {
+            System.out.println(e);
+        }
+    }
+}
