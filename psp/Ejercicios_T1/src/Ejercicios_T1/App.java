6cc122798f4d5eb9cb53922d7eabb01a86a0a068
Miguel Ángel <mabusta.06@gmail.com>
2025-09-28 20:46:02 +0200

    Add PSP T1 Ejercicio 31

---
+++ psp/Ejercicios_T1/src/Ejercicios_T1/App.java
@@ -0,0 +1,13 @@
+package Ejercicios_T1;
+
+public class App {
+    public void sayHello() {
+        System.out.println("Hola Món");
+    }
+
+    public static void main(String[] args) {
+        App aplicacion = new App();
+
+        aplicacion.sayHello();
+    }
+}
