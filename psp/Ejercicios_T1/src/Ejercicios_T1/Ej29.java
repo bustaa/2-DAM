307e1d07cc68faf0aa95b0c8244f011a020380bc
Miguel Ángel <mabusta.06@gmail.com>
2025-09-28 20:38:35 +0200

    Add PSP T1 Ejercicio 29 fix

--- psp/Ejercicios_T1/src/Ejercicios_T1/Ej29.java
+++ psp/Ejercicios_T1/src/Ejercicios_T1/Ej29.java
@@ -3,22 +3,6 @@ package Ejercicios_T1;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-class Vehiculo {
-    String tipo;
-    String marca;
-    String modelo;
-
-    public Vehiculo(String tipo, String marca, String modelo) {
-        this.tipo = tipo;
-        this.marca = marca;
-        this.modelo = modelo;
-    }
-
-    public void mostrar() {
-        System.out.println("Tipus: " + tipo + ", Marca: " + marca + ", Model: " + modelo);
-    }
-}
-
 public class Ej29 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
