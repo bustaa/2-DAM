3723898ec47d5f7ab7f4ba8e6c82d8a6b3592d58
Miguel Ángel <mabusta.06@gmail.com>
2025-09-28 19:31:46 +0200

    Add PSP T1 Ejercicio 19

---
+++ psp/Ejercicios_T1/src/Ejercicios_T1/Ej19.java
@@ -0,0 +1,10 @@
+package Ejercicios_T1;
+
+public class Ej19 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 128; i++) {
+            char caracter = (char) i;
+            System.out.println(i + " -> " + caracter);
+        }
+    }
+}
