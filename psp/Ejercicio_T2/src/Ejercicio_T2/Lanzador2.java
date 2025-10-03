7bd8004012fd11d314ad5a9143d583fb64abf63f
Miguel Angel <mabusta.06@gmail.com>
2025-10-03 20:01:46 +0200

    Add PSP T2 unfinished Lanzador2

---
+++ psp/Ejercicio_T2/src/Ejercicio_T2/Lanzador2.java
@@ -0,0 +1,33 @@
+package Ejercicio_T2;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.
+
+public class Lanzador2 {
+    public static void main(String[] args) throws IOException {
+        String javaHome = System.getProperty("java.home");
+        String javaBin = javaHome + File.separator + "bin" + File.separator + "java";
+        String classpath = System.getProperty("java.class.path");
+        String clase = "Ejercicio_T2.Sumador2";
+
+        int n1, n2;
+        n1 = 10;
+        n2 = 50;
+
+        List<String> command = new ArrayList<String>();
+        command.add(javaBin);
+        command.add("-cp");
+        command.add(classpath);
+        command.add(clase);
+        command.add(String.valueOf(n1));
+        command.add(String.valueOf(n2));
+
+        ProcessBuilder builder = new ProcessBuilder(command);
+        builder.start();
+        builder.waitFor();
+        if()
+    }
+}
