ce6cc956229521354ecef0c12b8894dbf3a4eb5c
Miguel Ángel <mabusta.06@gmail.com>
2025-10-19 20:48:10 +0200

    Add PSP T2 Lanzador2 finished

--- psp/Ejercicio_T2/src/Ejercicio_T2/Lanzador2.java
+++ psp/Ejercicio_T2/src/Ejercicio_T2/Lanzador2.java
@@ -1,23 +1,20 @@
 package Ejercicio_T2;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.
 
 public class Lanzador2 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         String javaHome = System.getProperty("java.home");
         String javaBin = javaHome + File.separator + "bin" + File.separator + "java";
         String classpath = System.getProperty("java.class.path");
         String clase = "Ejercicio_T2.Sumador2";
 
-        int n1, n2;
-        n1 = 10;
-        n2 = 50;
+        int n1 = 10;
+        int n2 = 50;
 
-        List<String> command = new ArrayList<String>();
+        List<String> command = new ArrayList<>();
         command.add(javaBin);
         command.add("-cp");
         command.add(classpath);
@@ -26,8 +23,32 @@ public class Lanzador2 {
         command.add(String.valueOf(n2));
 
         ProcessBuilder builder = new ProcessBuilder(command);
-        builder.start();
-        builder.waitFor();
-        if()
+        builder.inheritIO();
+        Process proceso = builder.start();
+
+        int exitCode = proceso.waitFor();
+        System.out.println("Proceso finalizado con código: " + exitCode);
+
+        File resFichero = new File("./res.txt");
+
+        int intentos = 0;
+        while ((!resFichero.exists() || resFichero.length() == 0) && intentos < 10) {
+            Thread.sleep(500);
+            intentos++;
+        }
+
+        if (!resFichero.exists()) {
+            System.out.println("Error: el fichero de resultado no se ha creado.");
+            return;
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(resFichero))) {
+            String linea = br.readLine();
+            if (linea != null) {
+                System.out.println("El resultado leído del fichero es: " + linea);
+            } else {
+                System.out.println("El fichero está vacío.");
+            }
+        }
     }
 }
